5381076945e90b5d5b557354b84e1aa450a42c70
Jesus Zeballos <iijandgii@gmail.com>
2026-01-08 14:19:52 -0400

    last prisma config : prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,7 @@
-import "dotenv/config";
 import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
     url: process.env.POSTGRES_URL,
   },
