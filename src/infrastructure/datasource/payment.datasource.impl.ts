1a4657ffa9081e65ad1f7457762f8722ca9049c6
Jesus Zeballos <iijandgii@gmail.com>
2026-01-20 10:30:26 -0400

    add(.env): the environment variables was added

--- src/infrastructure/datasource/payment.datasource.impl.ts
+++ src/infrastructure/datasource/payment.datasource.impl.ts
@@ -10,25 +10,25 @@ export class PaymentDatasourceImpl implements PaymentDatasource {
     const identificadorInterno = `POS-${Date.now()}`;
 
     const { data } = await axios.post(`${this.apiUrl}/deuda/registrar`, {
-      appkey: this.appKey, //
-      email_cliente: email, // Dinámico
-      identificador_deuda: identificadorInterno, // Único por cada intento
-      descripcion: descripcion, // Dinámico
-      lineas_detalle_deuda: items, // Ya mapeado con concepto, cantidad, costo_unitario [cite: 47]
+      appkey: this.appKey,
+      email_cliente: email,
+      identificador_deuda: identificadorInterno,
+      descripcion: descripcion,
+      lineas_detalle_deuda: items,
     });
     console.log(data);
-    return data; // Retorna qr_simple_url e id_transaccion
+    return data;
   }
 
   async consultarEstado(idRecaudacion: string) {
-    // Servicio oficial: Consulta por Identificador [cite: 661]
     const { data } = await axios.post(
       `${this.apiUrl}/deuda/consultar_deudas/por_identificador`,
       {
         appkey: this.appKey,
-        codigo_recaudacion: idRecaudacion, // Usamos el código numérico [cite: 666]
+        codigo_recaudacion: idRecaudacion, //? codigo numerico cite: 666
       },
     );
+    console.log(data);
     return data;
   }
 }
